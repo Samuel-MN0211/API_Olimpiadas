989e01f7bbc07b5ca3897db8553b9872f017cae7
Lucas Nobrega <105940701+lucas-pdnobrega@users.noreply.github.com>
2024-08-10 16:11:56 -0300

    update model class with converters, add filtered and regular getter

--- lib/core/db_controller.dart
+++ lib/core/db_controller.dart
@@ -9,27 +9,49 @@ class AwardDbController extends ChangeNotifier {
   CollectionReference _collectionRef =
     FirebaseFirestore.instance.collection('Searched_users');
 
-  Future<dynamic> getData() async {
+  List<dynamic> convertDocstoList(QuerySnapshot querySnapshot) {
+    return querySnapshot.docs.map((doc) =>
+    Award.fromFirestore(doc))
+    .toList();
+
+  }
+
+  Future<dynamic> getData(
+    
+  ) async {
       // Get docs from collection reference
-      QuerySnapshot querySnapshot = await _collectionRef.get();
+      QuerySnapshot querySnapshot = 
+        await _collectionRef
+        .get();
 
       // Get data from docs and convert map to List
-      final data = querySnapshot.docs.map((doc) => doc.data()).toList();
+      final data = convertDocstoList(querySnapshot);
 
       return data;
   }
 
+  Future<dynamic> getFilteredData(Map<String, dynamic> filters) async {
+    Query query = _collectionRef;
+    
+    filters.forEach((field, value) {
+      if (field == 'timestamp_to') {
+        query.where(field, isLessThanOrEqualTo: value);
+      }
+      if (field == 'timestamp_from') {
+        query.where(field, isGreaterThanOrEqualTo: value);
+      }
+      query.where(field, isEqualTo: value);
+    });
+
+    QuerySnapshot querySnapshot = await query.get();
+
+    final data = convertDocstoList(querySnapshot);
+
+    return data;
+  }
+
   Future<DocumentReference> addData(Award award) {
     return _collectionRef
-        .add(<String, dynamic>{
-      'Nome': award.name,
-      'Link': award.url,
-      'Olimpíada': award.olympiad,
-      'Cidade/Estado': award.city_state,
-      'Medalha': award.medal,
-      'Pontuação': award.score,
-      'Escola': award.school,
-      'TimeStamp': DateTime.now().millisecondsSinceEpoch,
-    });
+        .add(award.toFireStoreMap());
   }
 }
--- lib/core/model/award.dart
+++ lib/core/model/award.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Award {
   String name;
   String url;
@@ -6,20 +8,61 @@ class Award {
   double score;
   String? city_state;
   String medal;
+  DateTime timestamp;
 
   Award({
     required this.name,
     required this.url,
     required this.olympiad,
     this.school = '-',
-    this.score = 0.0, 
-    this.city_state = '-', 
+    this.score = 0.0,
+    this.city_state = '-',
     required this.medal,
+    required this.timestamp
   });
+
+  factory Award.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Award(
+      name: data['name'] ?? '',
+      url: data['url'] ?? '',
+      olympiad: data['olympiad'] ?? '',
+      school: data['school'],
+      score: data['score']?.toDouble() ?? 0.0,
+      city_state: data['city_state'],
+      medal: data['medal'] ?? '',
+      timestamp: data['timestamp'] ?? DateTime.timestamp()
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'url': url,
+      'olympiad': olympiad,
+      'school': school,
+      'score': score,
+      'city_state': city_state,
+      'medal': medal,
+      'timestamp': timestamp
+    };
+  }
+
+  Map<String, dynamic> toFireStoreMap() {
+    return {
+      'Nome': name,
+      'Link': url,
+      'Olimpíada': olympiad,
+      'Cidade/Estado': city_state,
+      'Medalha': medal,
+      'Pontuação': score,
+      'Escola': school,
+      'TimeStamp': DateTime.timestamp(),
+    };
+  }
 
   @override
   String toString() {
-    return 'Award(name: $name, url: $url, olympiad: $olympiad, school: $school, score: $score, city_state: $city_state, medal: $medal)';
+    return 'Award(name: $name, url: $url, olympiad: $olympiad, school: $school, score: $score, city_state: $city_state, medal: $medal, timestamp: $timestamp)';
   }
-
 }
